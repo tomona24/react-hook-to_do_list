dac7342bc5868fe977c8beb09b3702b957d44c57
Tomona Sako <sakotomona@gmail.com>
2020-05-15 13:14:24 -0700

    Updates

---
+++ precache-manifest.0b0b6ed8d8461afb746f02da6097bae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ae5a2ec3fdf8a2dc0042763ffa6982",
+    "url": "/react-hook-to_do_list/index.html"
+  },
+  {
+    "revision": "34b531680f0b97af9933",
+    "url": "/react-hook-to_do_list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f83cc63402533a77aeea",
+    "url": "/react-hook-to_do_list/static/js/2.54993773.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hook-to_do_list/static/js/2.54993773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b531680f0b97af9933",
+    "url": "/react-hook-to_do_list/static/js/main.863da668.chunk.js"
+  },
+  {
+    "revision": "264ecc33300ad9cc5ecb",
+    "url": "/react-hook-to_do_list/static/js/runtime-main.985c777d.js"
+  }
+]);
